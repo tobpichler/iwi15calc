053ef727aefd9c5fa1e55c020b7bb4b5f0894a69
local_user <local_user@SURFPRO-TPI.pichler.local>
2018-01-13 11:12:02 +0100

    add modulo to parser

--- stackCalculator/src/test/java/at/edu/c02/calculator/parser/ParserTest.java
+++ stackCalculator/src/test/java/at/edu/c02/calculator/parser/ParserTest.java
@@ -40,4 +40,19 @@ public class ParserTest {
 
 		verifyNoMoreInteractions(cal);
 	}
+	
+	@Test
+	public void testParserModulo() throws Exception {
+
+		Calculator cal = mock(Calculator.class);
+
+		Parser parser = new Parser(cal);
+		parser.parse(new File("src/test/resources/test01.xml"));
+		
+		verify(cal).push(1.0);
+		verify(cal).push(2.0);
+		verify(cal).perform(Operation.modulo);
+
+		verifyNoMoreInteractions(cal);
+	}
 }
