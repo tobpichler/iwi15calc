c4aec84fdc11b4991aee5fa6689b039563cc7e9a
David Kornhäusl <david.kornhaeusl@edu.campus02.at>
2018-01-13 11:13:52 +0100

    add test04.xml for parser test

--- stackCalculator/src/test/java/at/edu/c02/calculator/parser/ParserTest.java
+++ stackCalculator/src/test/java/at/edu/c02/calculator/parser/ParserTest.java
@@ -47,7 +47,7 @@ public class ParserTest {
 		Calculator cal = mock(Calculator.class);
 
 		Parser parser = new Parser(cal);
-		parser.parse(new File("src/test/resources/test01.xml"));
+		parser.parse(new File("src/test/resources/test04.xml"));
 		
 		verify(cal).push(1.0);
 		verify(cal).push(2.0);
